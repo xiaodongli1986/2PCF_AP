728153adc6b24209c41fa6ac352432635f3428db
xiaodongli <xiaodongli@kias.re.kr>
2017-06-08 16:15:43 +0900

    minor changes

--- src/AP_grid.f90
+++ src/AP_grid.f90
@@ -9,24 +9,29 @@ implicit none
   integer :: i,j,k,i1,i2,iz,num_MCMCchain,numomwstds,nlines
   
   
-  integer :: iomcol=1,iw0col=1,iwacol=1,iomkcol=1,iH0col=1,maxcol, ifile,iline
-  
   real(rt) :: omegam, omstds(1000), wstds(1000), DAs(nz), Hs(nz), & 
     chisqs_nosyscor(n1,n2,nz-1), chisqs_syscor(n1,n2,nz-1), chisqs_nosyscor_all(nz-1), chisqs_syscor_all(nz-1), &
-    tmpX(1000),nowlnlike,nowAPlnlike,nowweight,nowom,noww0,nowH0,nowwa,nowomk,APlnlikemin,&
-    t0,t1,t2,dt
+    nowlnlike,nowweight,nowom,noww0,nowH0,nowwa,nowomk,APlnlikemin,&
+    t0,t1,t2,dt, ommin, ommax, w0min, w0max, wamin, wamax
+  integer :: iline, numom, numw0, numwa, iom, iw0, iwa
   real(rt), allocatable :: APlnlikes(:), smutabstds(:,:,:,:,:)
-  character(charlen) :: inputMCMCfile, outputMCMCfile, mcmcdir, nowchisqstr, fileindexstr, MCMCfilestr, suffixstr=''
+  character(charlen) :: outputMCMCfile, mcmcdir, nowchisqstr, fileindexstr, MCMCfilestr, suffixstr=''
   type(omwpar) :: nowpar
   integer,parameter :: model_wcdm=3, model_cpl=4, model_owcdm=5, model_ocpl=6, model_lcdm=7, model_olcdm=8
   integer :: nowmodel
-  logical :: smutabstds_inited, debug=.false., avg_counts = .false., print_allinfo=.false.
+  logical :: smutabstds_inited, debug=.false., print_allinfo=.false.
 
 
-!  nowmodel = model_wcdm
-  nowmodel = model_cpl
+  ommin=0.1;  ommax=0.4; 
+  w0min=-1.4; w0max=-0.3; 
+  wamin=-2.5; wamax=0.9;
+  numom=120; numw0=120; numwa=120  
+
+!  nowmodel = model_wcdm;   wamin=0.0;wamax=0.0;numwa=1
+  nowmodel = model_cpl; ommin=0.31; ommax=0.31; numom=1
 !  nowmodel = model_olcdm
-  suffixstr = 'base1omws_om0.2600_w-1.0000_nbins26to27'
+
+  suffixstr = 'base1omws_om0.2600_w-1.0000_fixom0.31'
 !  suffixstr = 'base1omws_om0.2600_w-1.0000_ExcludeLastThreeBins_B'
 !  suffixstr = 'base1omws_om0.2600_w-1.0000_nbins35to40'
 !  suffixstr = 'base1omws_om0.2600_w-1.0000_smax40'
@@ -34,44 +39,24 @@ implicit none
 !  suffixstr = 'base1omws_om0.1100_w-2.0000'
 !  print_allinfo = .true.
   
+
+  
 !---------------------------------------------------------
   !--------------------------------
   ! Settings of the model
-  
+
+  mcmcdir = '/home/xiaodongli/software/cosmomcs/12Oct_generic/chains/wcdm/'
   if(nowmodel .eq. model_wcdm) then! .or. nowmodel .eq. model_owcdm) then
-	  de_model_lab  = de_wcdm_lab
-	  mcmcdir = '/home/xiaodongli/software/cosmomcs/12Oct_generic/chains/wcdm/plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA/'    
-          MCMCfilestr = 'base_w_plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA'	  
-          iw0col=5; iH0col=37; iomcol=39; 
+  	  de_model_lab = de_wCDM_lab
+          MCMCfilestr = 'base_w_AP'	  
   elseif(nowmodel .eq. model_cpl ) then
   	  de_model_lab = de_CPL_lab
-  	  mcmcdir = '/home/xiaodongli/software/cosmomcs/12Oct_generic/chains/wcdm/plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA/'    
-  	  MCMCfilestr = 'base_w_wa_plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA'
-  	  iw0col=5; iwacol=6; iH0col=38; iomcol=40; 
-  elseif(nowmodel .eq. model_olcdm ) then
-  	  de_model_lab = de_LCDM_lab
-  	  mcmcdir = '/home/xiaodongli/software/cosmomcs/12Oct_generic/chains/wcdm/plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA/'    
-  	  MCMCfilestr = 'base_omegak_plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA'
-  	  iw0col=1; iwacol=1; iH0col=37; iomcol=39; iomkcol=5;
+  	  MCMCfilestr = 'base_w_wa_AP'
   else
           print *, 'Wrong model : ', nowmodel
           stop
   endif
 
-!  mcmcdir = '/home/xiaodongli/software/cosmomcs/12Oct_generic/chains/wcdm/plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA/'    
-  
-  ! Special setting for CMB+BAO chain
-  !if(.false.) then
-  ! mcmcdir = '/home/xiaodongli/software/cosmomcs/12Oct_generic/chains/wcdm/plikHM_TTTEEE_lowTEB_BAO/'
-  ! MCMCfilestr = 'base_w_wa_plikHM_TTTEEE_lowTEB_BAO'; iw0col=5; iwacol=6; iH0col=36; iomcol=38; 
-  !endif
-!   MCMCfilestr = 'base_w_wa_plikHM_TTTEEE_lowTEB_BAO_H070p6_JLA_post_lensing'
-
-  maxcol=max(iw0col,iwacol,iH0col,iomcol,iomkcol)+2
-  num_MCMCchain = 4
-
-  ! End of settings
-  !--------------------------------
 
 !---------------------------------------------------------
   !--------------------------------
@@ -101,14 +86,10 @@ implicit none
 
 !---------------------------------------------------------  
   !--------------------------------
-  ! Loop of all MCMC files
-  do ifile = 1, num_MCMCchain
+  ! Scan of grid
+  if(.true.) then
 
-    ! File names
-    write(fileindexstr,*) ifile
-    fileindexstr = '_'//trim(adjustl(fileindexstr))//'.txt'
-    inputMCMCfile = trim(adjustl(mcmcdir))//'/'//trim(adjustl(MCMCfilestr))//trim(adjustl(fileindexstr))
-    if(avg_counts) fileindexstr = '_avg_counts'//trim(adjustl(fileindexstr)) 
+    fileindexstr = '_1.txt'
     if(trim(adjustl(suffixstr)).eq.'') &
       suffixstr = trim(adjustl( AP_MCMCstr(numomwstds, omstds(1:numomwstds), wstds(1:numomwstds)) ))
     outputMCMCfile = trim(adjustl(mcmcdir))//'/'//trim(adjustl(MCMCfilestr))//'___'//&
@@ -116,38 +97,32 @@ implicit none
 
     print *
     print *, '###################################################'
-    print *, '** Compuate AP chisqs from file: '
-    print *, '   ', trim(adjustl(inputMCMCfile))
+    print *, '** Compuate AP chisqs for: '
+    print *, '   ommin, ommax = ', ommin, ommax
+    print *, '   w0min, w0max = ', w0min, w0max
+    print *, '   wamin, wamax = ', wamin, wamax
     print *, '** Key-word: '
     print *, '   ', trim(adjustl( AP_MCMCstr(numomwstds, omstds(1:numomwstds), wstds(1:numomwstds)) ))
     print *, '** outputfile name: '
     print *, '   ', trim(adjustl(outputMCMCfile))
     
-    ! Open file and compute likelihoods...
-    call de_count_line_number (trim(adjustl(inputMCMCfile)), nlines); allocate(APlnlikes(nlines))
+    nlines = numom*numw0*numwa
+    allocate(APlnlikes(nlines))
+    
     print *, '** Computing ', nlines, 'chisqs...'
-    open(unit=3293,file=inputMCMCfile,action='read')
     iline = 1
     call cpu_time(t0); t1=t0; dt = 60.0;
-    ! Loop of all files
-    do while(.true.)
-      read(3293,*,end=100) tmpX(1:maxcol)
+
+    do iom = 1, numom
+    do iw0 = 1, numw0
+    do iwa = 1, numwa
       
       ! Begin model dependent
-      nowweight=tmpX(1); nowlnlike=tmpX(2); nowom=tmpX(iomcol+2); noww0=tmpX(iw0col+2); nowH0=tmpX(iH0col+2)
-      nowwa=tmpX(iwacol+2); nowomk=tmpX(iomkcol+2)
+      nowom=ommin + (ommax-ommin)/max(dble(numom-1),1.0d0)*(iom-1);
+      noww0=w0min + (w0max-w0min)/max(dble(numw0-1),1.0d0)*(iw0-1); 
+      nowwa=wamin + (wamax-wamin)/max(dble(numwa-1),1.0d0)*(iwa-1);
+      nowH0=70.0;nowomk=0.0
       
-      if(nowmodel.eq.model_wcdm .or. nowmodel.eq.model_owcdm .or. &
-      	 nowmodel.eq.model_lcdm .or. nowmodel.eq.model_olcdm) &
-      	    nowwa = 0.0; 
-      	    
-      if(nowmodel.eq.model_wcdm .or. nowmodel.eq.model_cpl   .or. &
-         nowmodel.eq.model_lcdm)  &
-            nowomk = 0.0; 
-            
-      if(nowmodel.eq. model_lcdm .or. nowmodel.eq.model_olcdm) &
-            noww0 = 0
-            
       ! Values of parameters
       de_CP%Ob0hsq  =  0.02253
       de_CP%h	    =  nowH0 / 100.0
@@ -184,7 +159,7 @@ implicit none
           smutabstds, smutabstds_inited, & ! xi(s,mu) table of baseline cosmologies
           chisqs_nosyscor, chisqs_syscor, & ! values of chisqs, separate schemes
           chisqs_nosyscor_all, chisqs_syscor_all, & ! values of chisqs, averaged over all schemes, correction factor for covmat (D, m1, m2) considered
-          weightedstds = .false., avg_counts = avg_counts &
+          weightedstds = .false., avg_counts = .false. &
           ) 
         APlnlikes(iline) = sum(chisqs_syscor_all(1:nz-1)) / 2.0 * (4.0/5.0)
 !        APlnlikes(iline) = sum(chisqs_nosyscor_all(1:nz-1)) / 2.0 * (4.0/5.0)
@@ -204,51 +179,42 @@ implicit none
       if (t2-t1.gt.dt.or.print_allinfo) then
         write(*,'(f10.1,A,i5,A,f4.1,A)') (t2-t0)/dt, ' minutes passed.   #-parameters = ', &
            iline, ' (',100*float(iline)/float(nlines),'%)'
-        write(*,'(A,e12.4,1x,f10.3,1x,6(f9.4))') '             Current set of wei / chi2 / APchi2 / par:  ', &
-          nowweight, nowlnlike, APlnlikes(iline), nowom, nowH0/100.0, noww0, nowwa, nowomk
+        write(*,'(A,1x,6(f9.4))') '             Current set of wei / chi2 / APchi2 / par:  ', &
+          APlnlikes(iline), nowom, nowH0/100.0, noww0, nowwa, nowomk
         t1=t2
       endif
       iline = iline +1
-      cycle
-100   exit 
     enddo
-    close(3293)
+    enddo
+    enddo
     
     APlnlikemin = minval(APlnlikes(1:nlines))
     
     ! write the values to new file
     print *, '  Write   AP chisqs to file: '
     print *, '   ', trim(adjustl(outputMCMCfile))
-    open(unit=3293,file=inputMCMCfile,action='read')
     open(unit=3294,file=outputMCMCfile,action='write')
     iline = 1
-    do while(.true.)
-      read(3293,*,end=101) tmpX(1:maxcol)
-      nowweight=tmpX(1); nowlnlike=tmpX(2); nowom=tmpX(iomcol+2); noww0=tmpX(iw0col+2); nowH0=tmpX(iH0col+2) ! model dependent
-      nowwa=tmpX(iwacol+2); nowomk=tmpX(iomkcol+2)
+    do iom = 1, numom
+    do iw0 = 1, numw0
+    do iwa = 1, numwa
+      ! Begin model dependent
+      nowom=ommin + (ommax-ommin)/max(dble(numom-1),1.0d0)*(iom-1);
+      noww0=w0min + (w0max-w0min)/max(dble(numw0-1),1.0d0)*(iw0-1); 
+      nowwa=wamin + (wamax-wamin)/max(dble(numwa-1),1.0d0)*(iwa-1);
+      nowH0=70.0;nowomk=0.0
       
-      if(nowmodel.eq.model_wcdm .or. nowmodel.eq.model_owcdm .or. &
-      	 nowmodel.eq.model_lcdm .or. nowmodel.eq.model_olcdm) &
-      	    nowwa = 0.0; 
-      	    
-      if(nowmodel.eq.model_wcdm .or. nowmodel.eq.model_cpl   .or. &
-         nowmodel.eq.model_lcdm)  &
-            nowomk = 0.0; 
-            
-      if(nowmodel.eq. model_lcdm .or. nowmodel.eq.model_olcdm) &
-            noww0 = 0
-            
-      nowAPlnlike = APlnlikes(iline)
-      nowweight = nowweight * exp(APlnlikemin - nowAPlnlike)
-      write(3294,'(7(e14.7,1x))') nowweight, nowlnlike+nowAPlnlike, &
+      nowlnlike = APlnlikes(iline)
+      nowweight = exp(APlnlikemin - nowlnlike)
+      write(3294,'(7(e14.7,1x))') nowweight, nowlnlike, &
         nowom, nowH0/100.0, noww0, nowwa, nowomk !model dependent
       iline = iline+1
-      cycle
-101   exit
-    enddo      
-    close(3293); close(3294)
+    enddo 
+    enddo
+    enddo     
+    close(3294)
     deallocate(APlnlikes)
-  enddo     
+  endif     
   
  
 !  nowpar%omegam = 0.06_rt; nowpar%w = -1.5_rt
